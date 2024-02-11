d49b5d430710eda3127aaa9b27290108174011ec
EricMTodd <eterictodd@gmail.com>
2024-02-10 21:41:46 -0700

    send help pls

--- main.cpp
+++ main.cpp
@@ -10,30 +10,24 @@ using namespace std;
 typedef unsigned int uint;
 
 uint partition(SortTester &tester, uint start, uint end) {
-////
-//insert partition function here
-////
-return 0;
+
 }
 
 void quickSort(SortTester &tester, uint start, uint end) {
-   ////
-   //insert QuickSort here
-   ////
+
 }
 
 int main() {
-	uint size = 20;
-	SortTester tester = SortTester(size);
-	cout<<"Unsorted"<<endl;
-	tester.print();
-	quickSort(tester, 0, size-1);
-	if (tester.isSorted()) {
-		cout<<"PASSED List Sorted (5 pts)"<<endl;
-	}
-	else {
-		tester.print();
-		cout<<"FAILED List not Sorted"<<endl;
-	}
-
+    uint size = 20;
+    SortTester tester = SortTester(size);
+    cout << "Unsorted" << endl;
+    tester.print();
+    quickSort(tester, 0, size - 1);
+    if (tester.isSorted()) {
+        cout << "PASSED List Sorted (5 pts)" << endl;
+    }
+    else {
+        tester.print();
+        cout << "FAILED List not Sorted" << endl;
+    }
 }
